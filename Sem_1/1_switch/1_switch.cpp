6014d8d1111f9110ce764626db76d01bcd06f425
brq <elifirebrq@gmail.com>
2024-11-13 17:37:13 +0300

    commit

---
+++ Sem_1/1_switch/1_switch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	double a, b;
+	cin >> a >> b;
+	int op;
+	cin >> op;
+	switch (op)
+	{
+	case 1:
+		cout << a + b;
+		break;
+	case 2:
+		cout << a - b;
+		break;
+	case 3:
+		cout << a * b;
+		break;
+	case 4:
+		if (b == 0)
+		{
+			cout << "На ноль делить нельзя!";
+		}
+		else
+		{
+			cout << a / b;
+		}
+		break;
+	default:
+		cout << "Неверный код операции!";
+		break;
+	}
+}
+
